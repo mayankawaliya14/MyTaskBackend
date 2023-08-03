38a2c7970c168a654963c1a9dd997b2cad89e28e
MAYANK AWALIYA <119251545+mayankawaliya14@users.noreply.github.com>
2023-08-03 21:26:11 +0530

    Changes1

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(express.json())
 
   app.use('/api/auth', require('./routes/auth'));
   app.use('/api/notes', require('./routes/notes'));
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
